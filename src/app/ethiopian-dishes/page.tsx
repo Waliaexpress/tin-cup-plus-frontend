73ac5d6b3e23160a5152f16f9bb5c155c4641bf0
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2025-05-20 00:02:59 +0300

    updates on menu listing

--- src/app/ethiopian-dishes/page.tsx
+++ src/app/ethiopian-dishes/page.tsx
@@ -18,19 +18,19 @@ export default function EthiopianLandingPage() {
       <Categories />
       {/* <DietaryTags /> */}
       <div className="flex flex-col gap-14">
-        <MenuItems isSpecial={true} />
-        <MenuItems title="Popular Dishes" isSpecial={false} />
+        <MenuItems title="Special Dishes" isSpecial={true} isTraditional={true} />
+        <MenuItems title="Popular Dishes" isSpecial={false} isTraditional={true} />
       </div>
       <div className="space-y-16 md:space-y-32 md:mx-16 mx-6">
               {traditionalFeatures.map((feature, index) => (
                 <FeatureSection
                   key={index}
-                  image={feature.image}
-                  title={feature.title}
-                  description={feature.description}
+                  image={feature?.image}
+                  title={feature?.title}
+                  description={feature?.description}
                   isReversed={index % 2 !== 0}
                   index={index}
-                  menuLink={feature.menuLink}
+                  menuLink={feature?.menuLink}
                 />
               ))}
             </div>
